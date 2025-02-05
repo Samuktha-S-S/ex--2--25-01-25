05fb69082311871ac05174cc9bf5ffffb08dc201
Samuktha-S-S <samukthasamuktha054@gmail.com>
2025-02-05 20:56:53 +0530

    Add files via upload

---
+++ qn5/greatest (1).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,num3;
+  num1=10;
+  num2=20;
+  num3=40;
+ if(num1>num2 && num1>num3)
+  {
+   printf("the num1 is %d greatest ",num1);
+   }
+   
+   else if(num2>num3)
+   {
+   printf("the num2 is %d greatest",num2);
+   }
+   else
+   {
+   printf("the numb3 is greatest");
+   }
+   return 0;
+ }
